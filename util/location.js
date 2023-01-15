59b2b3f312f0aa7b815aede28ee65b373410ed7c
May Oo <mayphyuoo1211@gmail.com>
2023-01-15 18:42:01 +1100

    prepare for deployment

---
+++ util/location.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const HttpError = require('../models/http-error');
+
+const API_KEY = process.env.GOOGLE_API;
+
+async function getCoordsForAddress(address) {
+	
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`);
+
+    const data = response.data;
+
+    if (!data || data.status === 'ZERO_RESULTS') {
+        throw new HttpError('Could not find location for the specified address.', 422);;
+    }
+
+    return data.results[0].geometry.location;
+}
+
+module.exports = getCoordsForAddress;
